b40fe173fb3f1a55dd9086f6d5e6cce6d7c30919
nishaadvirkud <nishaadvirkud@yahoo.com>
2019-04-14 12:57:41 +0530

    added logger

--- script.sh
+++ script.sh
@@ -2,12 +2,12 @@
 
 echo "[INFO] running script available in Jenkins in Github via Jenkins"
 
-ls /home/
+/bin/ls /home/ >> logs.txt
 
 echo "[INFO] list of users and home directories in server"
 
 echo "[INFO] archiving script.sh"
 
-tar -zcvf script.sh.gz script.sh
+/bin/tar -zcvf script.sh.gz script.sh >> logs.txt
 
 testing arching another version
